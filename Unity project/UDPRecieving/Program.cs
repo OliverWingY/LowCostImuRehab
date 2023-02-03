bf0e8c63acb81fc470ae98bafddd4f4d6e1ef0ef
Oliver Wing Young <mn18owy@leeds.ac.uk>
2023-02-03 15:42:39 +0000

    added things

---
+++ Unity project/UDPRecieving/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPRecieving // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        private static int portNumber = 17628;
+        private static UdpClient udpServer;
+        public static void Main()
+        {
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            Console.WriteLine("Starting");
+            udpServer = new UdpClient(portNumber);
+            var remoteEP = new IPEndPoint(IPAddress.Any, portNumber);
+            while (true)
+            {
+                var data = udpServer.Receive(ref remoteEP);
+                Console.WriteLine(ASCIIEncoding.ASCII.GetString(data));
+                
+            }
+            
+        }
+
+        private static void CurrentDomain_ProcessExit(object? sender, EventArgs e)
+        {
+            udpServer.Close();
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();            
+        }
+    }
+}
---
+++ Unity project/UDPSending/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace UDPSending
+{
+    public class Program
+    {
+        private static int portNumber = 17628;
+        private static Socket sock;
+        public static void Main()
+        {
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            Console.WriteLine("Starting");
+            var remoteEP = new IPEndPoint(IPAddress.Parse("10.41.39.105"), portNumber);
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,ProtocolType.Udp);
+            while (true)
+            {                
+                string readLine = Console.ReadLine();
+                var data = ASCIIEncoding.ASCII.GetBytes(readLine);
+                sock.SendTo(data, remoteEP);
+                Console.WriteLine("sent!");
+            }
+            
+        }
+
+        private static void CurrentDomain_ProcessExit(object? sender, EventArgs e)
+        {
+            sock.Close();
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
