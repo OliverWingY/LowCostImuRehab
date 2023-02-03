946c300a97f5fc9cf9476dc4c6c49bc4c2254363
OliverWingY <owingyoung@gmail.com>
2023-02-03 11:25:28 +0000

    bleh

---
+++ Unity project/Rehabilitation Arms/Assets/Scripts/Startup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UDPTinkering;
+
+//attaching this to the ground and using it as a startup script because I'm a hack who cant be bothered to read the documentation
+public class Startup : MonoBehaviour
+{
+    public static double[] CurrentPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
